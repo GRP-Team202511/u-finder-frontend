dbd72605cf27b8869d9ab7c0e82075068813ff98
Si Yu CHANG <siyuchang8@gmail.com>
2025-12-17 16:57:23 +0800

    Feature/user profile (#12)
    * feat: add Avatar UI component
    
    * feat: add Collapsible UI component
    
    * feat: add Dropdown Menu UI component
    
    * feat: add Sheet UI component
    
    * feat: add Sidebar UI component
    
    * feat: add Skeleton and Tooltip UI component
    
    * feat: add sidebar components
    
    * feat: add Sidebar view layout
    
    * feat: add sidebar localization strings
    
    * feat: add UserProfile page and update routing
    
    * chore: rename profile folder and update routing
    
    * Feature/profile edu background (#11)
    
    * feat: add Native Select UI components
    
    * feat: add Calendar UI component
    
    * feat: add Popover UI component
    
    * chore: update pnpm-lock.yaml with new dependencies
    
    * chore: update package.json with new dependencies
    
    * feat: implement user profile education background component
    
    * feat: add education background localization strings
    
    * feat: update EducationBackground to add editable draft with save/cancel
    
    * feat: update UserProfile with save and cancel functionality for education background
    
    * feat: add profile localization strings
    
    * feat: set editing state to false by default in UserProfile
    
    * feat: enhance education background display for view-mode

---
+++ u-finder/src/views/Sidebar.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+export const description
+  = "A sidebar that collapses to icons."
+export const iframeHeight = "800px"
+export const containerClass = "w-full h-full"
+</script>
+
+<script setup lang="ts">
+import AppSidebar from "@/components/sidebar/AppSidebar.vue"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <header class="flex h-0 shrink-0 items-center gap-2">
+        <div class="flex items-center gap-0 px-0">
+          <SidebarTrigger class="-ml-35" />
+        </div>
+      </header>
+
+      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <slot />
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
+</template>
