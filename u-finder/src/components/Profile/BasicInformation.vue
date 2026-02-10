14bef663332ddde631510fd566980b2068675bc7
Si Yu <siyuchang8@gmail.com>
2026-02-10 21:59:13 +0800

    refactor: remove unused buttons from BasicInformation component

--- u-finder/src/components/Profile/BasicInformation.vue
+++ u-finder/src/components/Profile/BasicInformation.vue
@@ -245,11 +245,6 @@ onMounted(async () => {
                     </PopoverContent>
                   </Popover>
                 </Field>
-
-                <div class="flex justify-end gap-2 mt-2">
-                  <Button type="button" variant="secondary" @click="removeEntry(idx)">Remove</Button>
-                  <Button type="button" @click="addEntry">Add</Button>
-                </div>
               </template>
             </FieldGroup>
           </form>
