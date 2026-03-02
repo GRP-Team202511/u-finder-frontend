1f58d5a3bbd9a7e336a266635deacd5e58356bca
Yao Xiao <scyyx10@nottingham.edu.cn>
2026-03-02 18:22:13 +0800

    feat: add Toaster component in App.vue

--- u-finder/src/App.vue
+++ u-finder/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { Toaster } from 'vue-sonner'
 import 'vue-sonner/style.css'
 </script>
 
 <template>
+  <Toaster position="bottom-right" :duration="3000" richColors />
   <div id="app">
     <router-view />
   </div>
