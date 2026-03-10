4f3f55b8b0f9e43f5be6a3fc1197e0767435c846
Si Yu <siyuchang8@gmail.com>
2026-03-10 18:51:16 +0800

    feat: integrate 2FA into login process

--- u-finder/src/views/auth/Login.vue
+++ u-finder/src/views/auth/Login.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
-import LoginForm from "@/components/auth/Login/LoginForm.vue"
-import LanguageSelector from "@/components/LanguageSelector.vue"
+import { ref } from "vue";
+import LoginForm from "@/components/auth/Login/LoginForm.vue";
+import TwoFactorForm from "@/components/auth/Login/TwoFactorForm.vue";
+import LanguageSelector from "@/components/LanguageSelector.vue";
+
+const tempToken = ref("");
+
+const handleTwoFactor = (token: string) => {
+  tempToken.value = token;
+};
+
 </script>
 
 <template>
@@ -11,7 +20,8 @@ import LanguageSelector from "@/components/LanguageSelector.vue"
   </div>
   <div class="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
     <div class="flex w-full max-w-sm flex-col gap-6">
-      <LoginForm />
+      <LoginForm v-if="!tempToken" @two-factor="handleTwoFactor" />
+      <TwoFactorForm v-else :temp-token="tempToken" />
     </div>
   </div>
 </template>
