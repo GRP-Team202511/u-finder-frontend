e2b3368547e2f04778f53c0cf52690f5ccbbfc20
Si Yu <siyuchang8@gmail.com>
2026-03-13 15:02:43 +0800

    chore: add Tailwind CSS and UI component library

--- u-finder-admin/src/main.ts
+++ u-finder-admin/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import './style.css'
 import piniaPersist from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
