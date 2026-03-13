e4001dad99ad09d9ef2b553a7fe37263d7bec03d
Si Yu <siyuchang8@gmail.com>
2026-03-13 14:22:49 +0800

    chore: integrate pinia-plugin-persistedstate for state persistence

--- u-finder-admin/src/main.ts
+++ u-finder-admin/src/main.ts
@@ -3,8 +3,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import piniaPersist from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
+pinia.use(piniaPersist)
 
 createApp(App).use(pinia).use(router).use(i18n).mount('#app')
 
